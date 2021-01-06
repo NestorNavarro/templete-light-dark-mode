f29fb927e01a5d0845c0fa604fe78b17a2d09d51
NestorNavarro <55806282+NestorNavarro@users.noreply.github.com>
2021-01-05 18:15:36 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,8 +19,8 @@ function getImages() {
 
 // Dark or Light Images
 function imageMode(color) {
-    arrayImages[0].src = `./templete-light-dark-mode/img/undraw_proud_coder_${color}.svg`;
-    arrayImages[1].src = `./templete-light-dark-mode/img/undraw_feeling_proud_${color}.svg`;
+    arrayImages[0].src = `./img/undraw_proud_coder_${color}.svg`;
+    arrayImages[1].src = `./img/undraw_feeling_proud_${color}.svg`;
     arrayImages[2].src = `./img/undraw_conceptual_idea_${color}.svg`;
 }
 
